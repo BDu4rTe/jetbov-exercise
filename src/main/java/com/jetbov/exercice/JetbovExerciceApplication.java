e43cd0c5233aa9cf9e1fc2224bf3867b8f3d42e1
BDu4rTe <brianschierduarte@outlook.com>
2024-02-23 09:33:16 -0300

    add springboot setup, docker files and readme

---
+++ src/main/java/com/jetbov/exercice/JetbovExerciceApplication.java
@@ -0,0 +1,13 @@
+package com.jetbov.exercice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetbovExerciceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetbovExerciceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetbov/exercice/JetbovExerciceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbov.exercice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetbovExerciceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
