1bc3084bea91cff17b85c1d44e17a051c60eb6fb
BDu4rTe <brianschierduarte@outlook.com>
2024-02-28 12:30:37 -0300

    write the dependency injection configuration

--- src/main/java/com/jetbov/exercice/infra/DiConfig.java
+++ src/main/java/com/jetbov/exercice/infra/DiConfig.java
@@ -1,4 +1,38 @@
 package com.jetbov.exercice.infra;
 
+import com.jetbov.exercice.adapters.AreaGateway;
+import com.jetbov.exercice.adapters.OxGateway;
+import com.jetbov.exercice.application.AreaServiceImpl;
+import com.jetbov.exercice.application.OxServiceImpl;
+import com.jetbov.exercice.core.services.AreaService;
+import com.jetbov.exercice.core.services.OxService;
+import com.jetbov.exercice.infra.gateways.AreaRepositoryGateway;
+import com.jetbov.exercice.infra.gateways.OxRepositoryGateway;
+import com.jetbov.exercice.infra.repositories.AreaRepository;
+import com.jetbov.exercice.infra.repositories.OxRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DiConfig {
+
+    @Bean
+    AreaGateway areaGateway(AreaRepository repository, GatewayHelper helper) {
+        return new AreaRepositoryGateway(repository, helper);
+    }
+
+    @Bean
+    AreaService areaService(AreaGateway gateway) {
+        return new AreaServiceImpl(gateway);
+    }
+
+    @Bean
+    OxGateway oxGateway(OxRepository repository, GatewayHelper helper) {
+        return new OxRepositoryGateway(repository, helper);
+    }
+
+    @Bean
+    OxService oxService(OxGateway gateway) {
+        return new OxServiceImpl(gateway);
+    }
 }
