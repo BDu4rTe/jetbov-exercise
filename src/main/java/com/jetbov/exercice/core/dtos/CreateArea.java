5f0bc4e6f6cf9941f7ea45c980b6a2b5d160f0cf
BDu4rTe <brianschierduarte@outlook.com>
2024-02-28 14:11:08 -0300

    adding the missing dto files from the core layer

---
+++ src/main/java/com/jetbov/exercice/core/dtos/CreateArea.java
@@ -0,0 +1,8 @@
+package com.jetbov.exercice.core.dtos;
+
+public record CreateArea(
+        String name,
+        float gmd,
+        int maxCapacity
+) {
+}
---
+++ src/main/java/com/jetbov/exercice/core/dtos/UpdateArea.java
@@ -0,0 +1,8 @@
+package com.jetbov.exercice.core.dtos;
+
+public record UpdateArea(
+        String name,
+        float gmd,
+        int maxCapacity
+) {
+}
