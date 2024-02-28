5ca38bd28c9d035993798da485d956d8e5339ab9
BDu4rTe <brianschierduarte@outlook.com>
2024-02-28 12:14:13 -0300

    exercise writing correction

--- src/main/java/com/jetbov/exercice/JetbovExerciseApplication.java
+++ src/main/java/com/jetbov/exercice/JetbovExerciseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JetbovExerciceApplication {
+public class JetbovExerciseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JetbovExerciceApplication.class, args);
+		SpringApplication.run(JetbovExerciseApplication.class, args);
 	}
 
 }
