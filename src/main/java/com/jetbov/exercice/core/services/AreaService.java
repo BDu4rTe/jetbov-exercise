7c1c25f2a79e97a8a1b8c609d869e1a5438f25af
BDu4rTe <brianschierduarte@outlook.com>
2024-02-28 14:07:26 -0300

    removed useless public access modifiers

--- src/main/java/com/jetbov/exercice/core/services/AreaService.java
+++ src/main/java/com/jetbov/exercice/core/services/AreaService.java
@@ -1,14 +1,16 @@
 package com.jetbov.exercice.core.services;
 
+import com.jetbov.exercice.core.dtos.CreateArea;
+import com.jetbov.exercice.core.dtos.UpdateArea;
 import com.jetbov.exercice.core.entities.Area;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface AreaService {
-    public void create(Area entity);
-    public List<Area> getAll();
-    public Area getById(UUID id);
-    public void update(UUID id, Area entity);
-    public void delete(UUID id);
+    void create(CreateArea dto);
+    List<Area> getAll();
+    Area getById(UUID id);
+    void update(UUID id, UpdateArea dto);
+    void delete(UUID id);
 }
--- src/main/java/com/jetbov/exercice/core/services/OxService.java
+++ src/main/java/com/jetbov/exercice/core/services/OxService.java
@@ -1,16 +1,16 @@
 package com.jetbov.exercice.core.services;
 
-import com.jetbov.exercice.core.entities.CreateOx;
+import com.jetbov.exercice.core.dtos.CreateOx;
 import com.jetbov.exercice.core.entities.Ox;
-import com.jetbov.exercice.core.entities.UpdateOx;
+import com.jetbov.exercice.core.dtos.UpdateOx;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface OxService {
-    public void create(CreateOx dto);
-    public List<Ox> getAll();
-    public Ox getById(UUID id);
-    public void update(UUID id, UpdateOx dto);
-    public void delete(UUID id);
+    void create(CreateOx dto);
+    List<Ox> getAll();
+    Ox getById(UUID id);
+    void update(UUID id, UpdateOx dto);
+    void delete(UUID id);
 }
